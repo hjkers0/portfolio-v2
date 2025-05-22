ec3452f01f14388049049ee786c390e65a9b4c7a
hjkers0 <hjkerstenne@gmail.com>
2025-05-22 13:09:16 +0200

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { useState } from 'react'
+import { Analytics } from "@vercel/analytics/next"
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
@@ -10,6 +11,7 @@ function App() {
     <div className="app-container">
       <div className="background-gradient"></div>
       <div className="content-wrapper">
+        <Analytics />
         <Header setLanguage={setLanguage}/>
         <Hero lang={language} />
         <About lang={language} />
