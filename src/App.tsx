45094ec9173bd579bf40460009e40f798e0c5385
hjkers0 <hjkerstenne@gmail.com>
2025-05-22 13:14:22 +0200

    deleted previous commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css'
 import { useState } from 'react'
-import { Analytics } from "@vercel/analytics/next"
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
@@ -11,7 +10,6 @@ function App() {
     <div className="app-container">
       <div className="background-gradient"></div>
       <div className="content-wrapper">
-        <Analytics />
         <Header setLanguage={setLanguage}/>
         <Hero lang={language} />
         <About lang={language} />
