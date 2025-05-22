a1aafd3b0d0926185bfb573b22330fbc9f8258a2
hjkers0 <hjkerstenne@gmail.com>
2025-05-22 13:23:45 +0200

    fixed analytics error

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
+import { Analytics } from '@vercel/analytics/react'
 function App() {
   const [language, setLanguage] = useState('es');
 
@@ -13,6 +14,7 @@ function App() {
         <Header setLanguage={setLanguage}/>
         <Hero lang={language} />
         <About lang={language} />
+        <Analytics />
       </div>
     </div>
   )
